ce26a4491cd4828fa7e197e4bedfffa5b3baa99a
EfimeroM <martinperez9713@gmail.com>
2024-01-24 19:59:12 -0300

    first update api blog

---
+++ config.js
@@ -0,0 +1,6 @@
+const dotenv = require("dotenv").config()
+
+module.exports = {
+  NODE_ENV : process.env.NODE_ENV || "development",
+  MONGO_URI : process.env.MONGO_URI || "mongodb://localhost:27017/mi_blog"
+}
---
+++ index.js
@@ -0,0 +1,5 @@
+const { connection } = require("./database/connection")
+
+console.log("Node App started!")
+
+connection()
