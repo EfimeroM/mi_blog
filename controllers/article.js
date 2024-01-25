a77bc3b49d72f6f861e6d5d82b9ba3ec845a6198
EfimeroM <martinperez9713@gmail.com>
2024-01-25 11:55:02 -0300

    feat: add create & get articles in controllers and routes

--- controllers/article.js
+++ controllers/article.js
@@ -11,9 +11,20 @@ const create = async (req, res) => {
   if (!savedArticle) {
     return res.status(400).json({ status: "error", message: "article don't saved" })
   }
-  return res.status(201).json({ status: "success", message: "Article created!", article: savedArticle })
+  return res.status(201).json({ status: "success", message: "Article added", article: savedArticle })
+}
+
+const get = async (req, res) => {
+  let quantity = req.params.quantity
+
+  let query = await Article.find().limit(quantity ? quantity : null).sort({ date: -1 }).exec()
+  if (!query) {
+    return res.status(404).json({ status: "error", message: "articles not found" })
+  }
+  return res.status(200).json({ status: "success", message: "Get all articles", articles: query, count: query.length })
 }
 
 module.exports = {
-  create
+  create,
+  get
 }
--- routes/article.js
+++ routes/article.js
@@ -4,6 +4,7 @@ const router = express.Router()
 const ArticleController = require("../controllers/article")
 
 router.post("/create", ArticleController.create)
+router.get("/articles/:quantity?", ArticleController.get)
 
 
 module.exports = router
