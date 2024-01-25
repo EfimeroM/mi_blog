7ef1889516be24e23c9c15bef0b1b15c7936409d
EfimeroM <martinperez9713@gmail.com>
2024-01-25 12:49:47 -0300

    feat: add getById & deleteById in controllers and routes

--- controllers/article.js
+++ controllers/article.js
@@ -24,7 +24,28 @@ const get = async (req, res) => {
   return res.status(200).json({ status: "success", message: "Get all articles", articles: query, count: query.length })
 }
 
+const getById = async (req, res) => {
+  let id = req.params.id
+  let query = await Article.findById(id)
+  if (!query) {
+    return res.status(404).json({ status: "error", message: "article not found" })
+  }
+  return res.status(200).json({ status: "success", message: "Get article by id", article: query, count: query.length })
+}
+
+const deleteById = async(req, res) =>{
+  let id = req.params.id
+  let query = await Article.findOneAndDelete({ _id: id}).exec()
+  
+  if (!query) {
+    return res.status(500).json({ status: "error", message: "Error to delete article" })
+  }
+  return res.status(200).json({ status: "success", message: "Deleted article", article: query })
+}
+
 module.exports = {
   create,
-  get
+  get,
+  getById,
+  deleteById
 }
--- routes/article.js
+++ routes/article.js
@@ -5,6 +5,8 @@ const ArticleController = require("../controllers/article")
 
 router.post("/create", ArticleController.create)
 router.get("/articles/:quantity?", ArticleController.get)
+router.get("/article/:id", ArticleController.getById)
+router.delete("/article/:id", ArticleController.deleteById)
 
 
 module.exports = router
